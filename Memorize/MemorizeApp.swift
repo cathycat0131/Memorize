16cc63ffe70e76c985820687f9a7f84ad352af4a
Cathy Chen <cathycat0131@gmail.com>
2022-06-07 09:17:56 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Cathy Chen on 2022-06-07.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f8cca2dd2c860e7ebffa3edcbe627a302340e33f
Cathy Chen <cathycat0131@gmail.com>
2022-06-08 09:15:09 -0700

    Initialize the project based on iOS swiftUI tutorial

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return ZStack() {
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3)
+                .padding(.horizontal)
+            Text("Hello, world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
